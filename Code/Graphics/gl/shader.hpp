4fa3c0a7c60dcbd9c9c031d71547bbf8f4e27d3a
naevidev <iamadrianiusca@gmail.com>
2023-12-28 14:08:49 +0200

    Added Shader class

---
+++ Code/Graphics/gl/shader.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace gl
+{
+    class Shader
+    {
+    public:
+        Shader();
+
+        void create(int32_t type);
+        void release();
+        void compile();
+
+        void source(const char* data);
+
+        [[nodiscard]] uint32_t handle() const;
+
+    private:
+        void status();
+
+        uint32_t _handle;
+    };
+}
